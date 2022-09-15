ffdd347b7d18a76aeb26d61a019f010dab4c6578
Kaden McLaws <kadenmclaws@gmail.com>
2022-09-15 14:00:33 -0400

    dist included to test

---
+++ dist/components/Drift/index.d.ts
@@ -0,0 +1,8 @@
+/// <reference types="react" />
+interface Props {
+    color?: string;
+    icon?: 'A' | 'B' | 'C' | 'D';
+    iconUrl?: string;
+}
+declare const Drift: ({ color, icon, iconUrl, }: Props) => JSX.Element | null;
+export default Drift;
